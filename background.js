03a3e070b1647d2826d2a5b06461ad09463c6a82
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-12-27 12:48:23 +0300

    enable the extension only in specified url

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
+        pageUrl: {pathContains: '/tasks/'},
       })
       ],
       actions: [new chrome.declarativeContent.ShowPageAction()]
