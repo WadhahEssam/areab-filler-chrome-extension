859e18c350a11bdeb308ba81969e07a57b88bddf
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-12-28 14:18:12 +0300

    fixing the selector problem

--- contentScript.js
+++ contentScript.js
@@ -14,11 +14,17 @@ function setNativeValue(element, value) {
   }
 }
 
+// things i will have to do tomorrow 
+// check the all the types 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
       console.log(sender.tab ?
                   "from a content script:" + sender.tab.url :
                   request.greeting);
+
+
+      // this will print the label of the input
+      console.log(document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(5) > div:nth-child(1) > div > input').parentElement.parentElement.children[0].textContent);
 
       // رقم التكليف
       var element = document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(1) > div > input')
@@ -31,16 +37,16 @@ chrome.runtime.onMessage.addListener(
       element.dispatchEvent(new Event('input', { bubbles: true }));
 
       // تاريخ التكليف
-      setTimeout(function() {
-        document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(3) > div > div.react-datepicker-wrapper > div > input').click();
-        document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(3) > div > div.react-datepicker-popper > div > div.react-datepicker__month-container > div.react-datepicker__month > div:nth-child(2) > div.react-datepicker__day.react-datepicker__day--tue').click();
-      }, Math.floor(Math.random() * 1000));
+      document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(3) > div > div.react-datepicker-wrapper > div > input').click();
+      document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(3) > div > div.react-datepicker-popper > div > div.react-datepicker__month-container > div.react-datepicker__month > div:nth-child(2) > div.react-datepicker__day.react-datepicker__day--tue').click();
 
       // تاريخ التسليم
-      setTimeout(function() {
-        document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(4) > div > div > div > div > input').click();
-        document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(4) > div > div > div.react-datepicker-popper > div > div.react-datepicker__month-container > div.react-datepicker__month > div:nth-child(5) > div.react-datepicker__day.react-datepicker__day--sat.react-datepicker__day--weekend').click();
-      }, Math.floor(Math.random() * 1000));
+      document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(4) > div > div > div > div > input').click();
+      document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(4) > div > div > div.react-datepicker-popper > div > div.react-datepicker__month-container > div.react-datepicker__month > div:nth-child(5) > div.react-datepicker__day.react-datepicker__day--sat.react-datepicker__day--weekend').click();
+
+
+      // المنطقة و المدينة و الحي
+      document.getElementById('fill-regions').click()
 
       sendResponse({farewell: "this will appear in the popup console"});
   });
