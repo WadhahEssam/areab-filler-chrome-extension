99422bae022e2a88c49b32b744cc05e27a3ea07d
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2018-12-29 19:14:20 +0300

    creating a usable version of the extension
    all what is left is to get the right values that they want

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,5 @@
-console.log('hello this is perfectly working fine and everything is working as expected')
+
+/////////////////////////////////////////////////////////// Values ///////////////////////////////////////////////////////////////
 
 var values = [
   {
@@ -6,28 +7,33 @@ var values = [
     value: "TEST"
   },
   {
+    type: "",
+    value: "Date"
+  },
+  {
     type: "رقم التكليف",
     value: Math.floor(Math.random() * 9999999) + ""
   },
   {
     type: "رمز العقار",
-    value: Math.floor(Math.random() * 9999999) + ""
-  },
-  {
-    type: "تاريخ التكليف",
-    value: "Date"
+    value: "رمز العقاار"
   },
 ];
 
 
 function getValue(type) {
   for (var i = 0; i < values.length; i++) {
-    if (values[i].type === type) {
+    if (values[i].type.trim() == type.trim()) {
+      console.log(values[i].value);
       return (values[i].value);
     }
   }
-  return 'not found';
+  // if it didin't find it will return the default value
+  return values[0].value;
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 function setNativeValue(element, value) {
   const { set: valueSetter } = Object.getOwnPropertyDescriptor(element, 'value') || {}
@@ -43,8 +49,7 @@ function setNativeValue(element, value) {
   }
 }
 
-// things i will have to do tomorrow 
-// check the all the types 
+// this code will perform when you click the fill button
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
       console.log(sender.tab ?
@@ -54,32 +59,26 @@ chrome.runtime.onMessage.addListener(
       var allInputs = document.querySelectorAll('input.form-control');
       for (var i = 0; i < allInputs.length; i++) {
         var type = allInputs[i].parentElement.parentElement.children[0].textContent;
-        setNativeValue(allInputs[i],  getValue('افتراضي'));
-        allInputs[i].dispatchEvent(new Event('input', { bubbles: true }));
+        var value = getValue(type);
+        console.log('type : ' + type + ' / value : ' + value);
+
+        if (value == 'Date') {
+          console.log('this is a date');
+          allInputs[i].click();
+          var weeks = allInputs[i].parentElement.parentElement.parentElement.children[1].children[0].children[3].children[1].children;
+          var days = weeks[weeks.length - 1].children;
+          var lastDay = days[days.length-1];
+          lastDay.click();
+        } 
+        else {
+          setNativeValue(allInputs[i],  value);
+          allInputs[i].dispatchEvent(new Event('input', { bubbles: true }));
+        }
       }
 
       // this will print the label of the input
       console.log(document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(5) > div:nth-child(1) > div > input').parentElement.parentElement.children[0].textContent);
-
-      // رقم التكليف
-      var element = document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(1) > div > input')
-      setNativeValue(element,  Math.floor(Math.random() * 9999999) + "");
-      element.dispatchEvent(new Event('input', { bubbles: true }));
-
-      // رمز العقار
-      var element = document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(2) > div > input')
-      setNativeValue(element,  "رمز العقار");
-      element.dispatchEvent(new Event('input', { bubbles: true }));
-
-      // تاريخ التكليف
-      document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(3) > div > div.react-datepicker-wrapper > div > input').click();
-      document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(3) > div > div.react-datepicker-popper > div > div.react-datepicker__month-container > div.react-datepicker__month > div:nth-child(2) > div.react-datepicker__day.react-datepicker__day--tue').click();
-
-      // تاريخ التسليم
-      document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(4) > div > div > div > div > input').click();
-      document.querySelector('#page-wrapper > div.overflow-container > form > div > div.row.ibox-content.arabic > div.formSection.property_data > div:nth-child(2) > div:nth-child(4) > div > div > div.react-datepicker-popper > div > div.react-datepicker__month-container > div.react-datepicker__month > div:nth-child(5) > div.react-datepicker__day.react-datepicker__day--sat.react-datepicker__day--weekend').click();
-
-
+      
       // المنطقة و المدينة و الحي
       document.getElementById('fill-regions').click()
 
